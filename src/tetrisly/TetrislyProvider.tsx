e8dcbf3a7177ac4f95b485ee708ebbb3daacf1c2
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-07-13 14:15:01 +0200

    fix(xstyled): fix build error and remove Global Styles

--- src/tetrisly/TetrislyProvider.tsx
+++ src/tetrisly/TetrislyProvider.tsx
@@ -1,26 +1,22 @@
 import { Preflight, ThemeProvider } from "@xstyled/styled-components";
 import { theme } from "../theme";
 import { PropsWithChildren } from "react";
-import { GlobalStyles } from "../GlobalStyles.styled";
 import { Theme } from "@xstyled/styled-components";
 import { TwoPartial } from "../utility-types/TwoPartial";
 import { mergeObjects } from "../services/mergeObjects";
 
 type ProviderProps = PropsWithChildren<{
   theme?: TwoPartial<Theme>;
-  disableGlobalStyles?: boolean;
 }>;
 
 export const TetrislyProvider = ({
   children,
   theme: overrideTheme,
-  disableGlobalStyles = false,
 }: ProviderProps) => {
   const providerTheme =
     overrideTheme === undefined ? theme : mergeObjects(theme, overrideTheme);
   return (
     <ThemeProvider theme={providerTheme}>
-      {!disableGlobalStyles && <GlobalStyles />}
       <Preflight />
       {children}
     </ThemeProvider>
