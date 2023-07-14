d8138a81e93fdabb4e3af27a6ceb3c126e890487
Adrian Potępa <adrian.potepa@gmail.com>
2023-07-14 15:06:00 +0200

    feat: theme tokens update after figma variables release

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,26 +1,2 @@
-import { defaultTheme as xDefaultTheme } from "@xstyled/system";
-
-import { theme as defaultTheme } from "./theme";
-
-const { screens, states } = xDefaultTheme;
-
-const ringWidths = defaultTheme.borderWidths;
-
-const themeExtension = {
-  screens,
-  states: {
-    ...states,
-    disabled: '&:disabled, &[aria-disabled=true], &[data-state="disabled"]',
-    selected: '&[data-state="selected"]',
-    loading: '&[data-state="loading"]',
-    alert: '&:invalid, &[data-state="alert"]',
-  },
-  ringWidths,
-};
-
-export const theme = {
-  ...themeExtension,
-  ...defaultTheme,
-};
-
-export type Theme = typeof theme;
+export { theme } from "./theme";
+export type { Theme } from "./Theme.interface";
