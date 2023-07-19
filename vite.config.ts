bac8d810b8576c32c524c2dbc6b7eb0bbffee7da
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-07-19 13:02:22 +0200

    fix: change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   plugins: [
     react(),
     dts({
