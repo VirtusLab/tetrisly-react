3e72517da3f1687332d3329e9fb13a90c3e95b09
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-07-23 14:21:05 +0200

    feat: type correctly config and variant in stylesBuilder

--- src/services/stylesBuilder/stylesBuilder.ts
+++ src/services/stylesBuilder/stylesBuilder.ts
@@ -1,96 +1,43 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
-// import { SystemProps } from '@xstyled/styled-components';
-import { SystemProps } from '@xstyled/styled-components';
 import { merge } from 'lodash';
 
 import { baseKeys } from './baseKeys';
-import { fromEntries } from '../fromEntries';
 
 import { BaseProps } from '@/types/BaseProps';
 import { DeepPartial } from '@/utility-types/DeepPartial';
 
-// import { fromEntries } from '@/services/fromEntries';
-// import { BaseProps } from '@/types/BaseProps';
-// import { DeepPartial } from '@/utility-types/DeepPartial';
-
-// const keys = <T extends object>(obj: T) => Object.keys(obj) as Array<keyof T>;
+type BaseKeys = (typeof baseKeys)[number];
 
 const entries = <T extends object>(obj: T) =>
   Object.entries(obj) as Array<[keyof T, T[keyof T]]>;
 
-type Config<T extends object> = BaseProps & {
-  [K in keyof T]: T[K] extends string | boolean
-    ? { [P in T[K]]: SystemProps }
-    : Config<T[K]>;
+type OnlyOne<Keys extends PropertyKey, T> = {
+  [key in Keys]: { [k in key]: T };
+}[Keys];
+
+type Variant<T extends object> = {
+  [K in Exclude<keyof T, BaseKeys>]: keyof T[K][keyof T[K]] extends BaseKeys
+    ? keyof T[K]
+    : T[K] extends object
+    ? OnlyOne<keyof T[K], Variant<T[K][keyof T[K]]>>
+    : never;
 };
 
-// export const stylesBuilder = <
-//   T extends Record<string, any> & Record<K, object>,
-//   K extends string
-// >(
-//   variants: T,
-//   origConfig: Config<T>,
-//   custom?: DeepPartial<Config<T>>,
-//   nested: K[] = []
-// ): Record<'styles', BaseProps> & {
-//   [P in K]: BaseProps;
-// } => {
-//   const config = merge(origConfig, custom);
-//   const styles = fromEntries(
-//     entries(config as BaseProps).filter(
-//       ([key]) => !Object.keys(variants).includes(key)
-//     )
-//   ) as BaseProps;
-
-//   if (nested.length !== 0) {
-//     nested.forEach((key) => {
-//       if (Object.keys(variants).includes(key)) {
-//         throw new Error(`Missing key "${key}" in variants`);
-//       }
-//     });
-//   }
-
-//   const nestedStyles = nested.reduce(
-//     (acc, key) => ({
-//       ...acc,
-//       ...stylesBuilder(variants[key], config[key], custom?.[key]).styles,
-//     }),
-//     {} as {
-//       [P in K]: BaseProps;
-//     }
-//   );
-
-//   const variantStyles = entries(variants).reduce((acc, [key, value]) => {
-//     if (typeof value === 'string') {
-//       return { ...acc, ...config[key][value] };
-//     }
-//     if (typeof value === 'boolean') {
-//       return value ? { ...acc, ...config[key] } : acc;
-//     }
-//     return { ...acc, ...stylesBuilder(value, config[key]).styles };
-//   }, {} as BaseProps);
-
-//   return { styles: { ...styles, ...variantStyles } };
-// };
-
-type StylesBuilderProps<
-  T extends object,
-  TCustom extends DeepPartial<Config<T>>
-> = {
-  config: Config<T>;
-  variant: T;
+type StylesBuilderProps<T extends object, TCustom extends DeepPartial<T>> = {
+  config: BaseProps & T;
+  variant: Variant<T>;
   custom?: TCustom;
 };
 
 export const stylesBuilder = <
   T extends object,
-  TCustom extends DeepPartial<Config<T>>
+  TCustom extends DeepPartial<T>
 >({
   config: origConfig,
   variant: variantConfig,
   custom,
 }: StylesBuilderProps<T, TCustom>): { styles: BaseProps } => {
-  const config = merge(origConfig, custom || {}) as Config<T>;
+  const config = merge(origConfig, custom || {}) as T;
   const { styles, rest } = spltStyles(config);
 
   const variantStyles = extractVariant(rest, variantConfig);
@@ -103,27 +50,45 @@ export const stylesBuilder = <
   };
 };
 function extractVariant<T extends object>(
-  config: Config<T>,
-  variant: T
+  config: T,
+  variant: Variant<T>
 ): BaseProps {
   const variantStyles = entries(variant).reduce((acc, [key, value]) => {
     if (typeof value === 'string') {
-      return { ...acc, ...config[key][value] };
+      return {
+        ...acc,
+        ...config[key][value as keyof (typeof config)[typeof key]],
+      };
     }
     if (typeof value === 'boolean') {
       return value ? { ...acc, ...config[key] } : acc;
     }
+    if (typeof value !== 'object' && value !== null) {
+      throw new Error(
+        `value for the key: ${key.toString()} in variants is not an object`
+      );
+    }
+    if (!isObject(config[key])) {
+      throw new Error(
+        `value for the key: ${key.toString()} in config is not an object`
+      );
+    }
+    type NewConfig = (typeof config)[typeof key] & object;
     return {
       ...acc,
       ...stylesBuilder({
-        config: config[key],
-        variant: value,
+        config: config[key] as NewConfig,
+        variant: value as Variant<NewConfig>,
       }).styles,
     };
   }, {} as BaseProps);
   return variantStyles;
 }
-function spltStyles<T extends object>(config: Config<T>) {
+
+function isObject(value: unknown): value is object {
+  return typeof value === 'object' && value !== null;
+}
+function spltStyles<T extends object>(config: T) {
   const styles = Object.fromEntries(
     Object.entries(config).filter(([key]) =>
       (baseKeys as string[]).includes(key)
@@ -133,7 +98,7 @@ function spltStyles<T extends object>(config: Config<T>) {
     Object.entries(config).filter(
       ([key]) => !(baseKeys as string[]).includes(key)
     )
-  ) as Exclude<Config<T>, BaseProps>;
+  ) as Exclude<T, BaseProps>;
 
   return {
     styles,
