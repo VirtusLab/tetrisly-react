b681d952f3e560151333f9e6f84bc9b6da1d10a3
matuesz-kleszcz <kleeeszcz@gmail.com>
2023-07-31 09:45:40 +0200

    fix text formatting

--- src/components/Badge/Badge.styles.ts
+++ src/components/Badge/Badge.styles.ts
@@ -44,18 +44,21 @@ export const config = {
         basicIntent,
         {
           high: {
-            color: `content-primary${basicIntent !== 'warning' ? '-inverted' : ''
-              }`,
+            color: `content-primary${
+              basicIntent !== 'warning' ? '-inverted' : ''
+            }`,
             backgroundColor: `background-${basicIntent}-strong`,
           },
           medium: {
-            color: `content-${basicIntent !== 'neutral' ? `${basicIntent}-` : ''
-              }primary`,
+            color: `content-${
+              basicIntent !== 'neutral' ? `${basicIntent}-` : ''
+            }primary`,
             backgroundColor: `background-${basicIntent}-muted`,
           },
           low: {
-            color: `content-${basicIntent !== 'neutral' ? `${basicIntent}-` : ''
-              }-primary`,
+            color: `content-${
+              basicIntent !== 'neutral' ? `${basicIntent}-` : ''
+            }-primary`,
             backgroundColor: `background-${basicIntent}-onSubtle`,
           },
         },
