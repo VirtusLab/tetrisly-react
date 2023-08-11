bf60c624740132a23cf7ad3e622b7456c0afeb1e
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-08-11 14:19:53 +0200

    chore: center story

--- src/components/Avatar/Avatar.stories.tsx
+++ src/components/Avatar/Avatar.stories.tsx
@@ -2,9 +2,21 @@ import { Meta, StoryObj } from '@storybook/react';
 
 import { Avatar } from './Avatar';
 import { avatarAppearances } from './AvatarAppearances';
+import { tet } from '@/tetrisly';
 
 const meta = {
   component: Avatar,
+  decorators: [
+    (Story) => (
+      <tet.div
+        display="flex"
+        justifyContent="center"
+        p="component-padding-large"
+      >
+        <Story />
+      </tet.div>
+    ),
+  ],
 } satisfies Meta<typeof Avatar>;
 
 export default meta;
@@ -13,7 +25,7 @@ type Story = StoryObj<typeof Avatar>;
 export const Image: Story = {
   args: {
     img: {
-      src: 'https://s3-alpha-sig.figma.com/img/cb99/66a9/febdebc9b4448067c48c4c3ec18e1b11?Expires=1690761600&Signature=DLwDbJ0lgvoDbeKHt39jxTn0PYgIE9n9fBw7GQiL4i90ZFkShisjffyiGFAQejpGiuR4Z7R1DxKEThE3JdeNW2XOn~a5Ebt73WGSAy122OpxUbD4QwMDpFv5kx4BYNRKAlm6JatDiwMzjKkK~1oURn0AlsXLKoj64M3hVpMBRlJ4VrGbyach4FVRPpqXhyJy0Xgb6htBEHw31ZLdcRHCsat~jqmEdDWxTScUaUiXo3jgQiAH9cipn5lZreJ2jwXKjaCLIUjqYTPWs1a8SHob4zyI5Yh9t5T9uU3Ou7oamEGmsu157cpLA3Ad9kp6Y5l3tRo9VnS3LG3J3NJUhxObWw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+      src: 'https://thispersondoesnotexist.com/',
     },
     appearance: 'image',
   },
--- src/docs-components/AvatarDocs.tsx
+++ src/docs-components/AvatarDocs.tsx
@@ -62,7 +62,7 @@ export const AvatarDocs = () => (
                     appearance="image"
                     shape={shape}
                     img={{
-                      src: 'https://s3-alpha-sig.figma.com/img/cb99/66a9/febdebc9b4448067c48c4c3ec18e1b11?Expires=1690761600&Signature=DLwDbJ0lgvoDbeKHt39jxTn0PYgIE9n9fBw7GQiL4i90ZFkShisjffyiGFAQejpGiuR4Z7R1DxKEThE3JdeNW2XOn~a5Ebt73WGSAy122OpxUbD4QwMDpFv5kx4BYNRKAlm6JatDiwMzjKkK~1oURn0AlsXLKoj64M3hVpMBRlJ4VrGbyach4FVRPpqXhyJy0Xgb6htBEHw31ZLdcRHCsat~jqmEdDWxTScUaUiXo3jgQiAH9cipn5lZreJ2jwXKjaCLIUjqYTPWs1a8SHob4zyI5Yh9t5T9uU3Ou7oamEGmsu157cpLA3Ad9kp6Y5l3tRo9VnS3LG3J3NJUhxObWw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+                      src: 'https://thispersondoesnotexist.com/',
                     }}
                   />
                 )}
