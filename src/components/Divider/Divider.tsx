8af17e45e00293b38ded8eae9c0dc705dfd7746a
Mateusz Kleszcz <mateusz.kacper.kleszcz@gmail.com>
2023-08-17 12:56:39 +0200

    feat: divider (#20)
    * feat: add divider
    
    * fix: review changes, removed WithCustom
    
    * fix: fix types

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,29 @@
+import { merge } from 'lodash';
+
+import { DividerProps } from './Divider.props';
+import { config as defaultConfig } from './Divider.styles';
+
+import { tet } from '@/tetrisly';
+import { MarginProps } from '@/types/MarginProps';
+
+export const Divider = ({
+  orientation = 'horizontal',
+  width,
+  height,
+  custom = {},
+  ...restProps
+}: DividerProps & MarginProps) => {
+  const { orientation: orientationStyles, ...restStyles } = merge(
+    defaultConfig,
+    custom
+  );
+  return (
+    <tet.hr
+      {...restStyles}
+      {...restProps}
+      w={width ?? orientationStyles[orientation].w}
+      h={height ?? orientationStyles[orientation].h}
+      data-testid="divider"
+    />
+  );
+};
