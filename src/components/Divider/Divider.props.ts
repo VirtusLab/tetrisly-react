b9a0d18af7ea8012d77c54f4f2a64132c55b7c66
matuesz-kleszcz <kleeeszcz@gmail.com>
2023-08-17 12:53:09 +0200

    fix: fix types

--- src/components/Divider/Divider.props.ts
+++ src/components/Divider/Divider.props.ts
@@ -8,11 +8,13 @@ import { DeepPartial } from '@/utility-types/DeepPartial';
 export type DividerProps = (
   | {
       orientation: 'vertical';
+      width?: never;
       height?: SystemProp<string | number | object | (string & object), Theme>;
     }
   | {
       orientation?: 'horizontal';
       width?: SystemProp<string | number | object | (string & object), Theme>;
+      height?: never;
     }
 ) & {
   custom?: DeepPartial<SystemProps<Theme> & typeof config>;
