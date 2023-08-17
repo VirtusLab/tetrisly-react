ce8498eebdf810c2de8a864453e252fc8685a87e
Mateusz Kleszcz <mateusz.kacper.kleszcz@gmail.com>
2023-08-17 12:28:33 +0200

    Feat/862k40hgp/badge (#17)
    * feat: added badge
    
    * add proper formatting
    
    * fix: add custom props
    
    * fix: fix color types
    
    * fix: fix color types
    
    * fix text formatting
    
    * fix types
    
    * fix types
    
    * fix: fix index.ts and code formatting
    
    * fix: review fixes
    
    * fix: add all files to commit changes
    
    * feat: add badge tests

---
+++ src/components/Badge/Badge.styles.ts
@@ -0,0 +1,82 @@
+import { SystemProps } from '@xstyled/styled-components';
+
+import { fromEntries } from '@/services/fromEntries';
+import { Theme } from '@/theme';
+import type { BasicAppearance, BasicIntent } from '@/types';
+import { basicAppearances } from '@/types/BasicAppearance';
+import { basicIntents } from '@/types/BasicIntent';
+
+type Config = {
+  appearance: Record<
+    BasicAppearance,
+    Record<'high' | 'medium' | 'low', SystemProps>
+  >;
+  intent: Record<BasicIntent, Record<'high' | 'medium' | 'low', SystemProps>>;
+  label: SystemProps;
+  icon: SystemProps;
+};
+
+export const config = {
+  appearance: {
+    ...fromEntries(
+      basicAppearances.map((basicAppearance) => [
+        basicAppearance,
+        {
+          high: {
+            color: 'nonSemantic-white-content-primary',
+            backgroundColor: `nonSemantic-${basicAppearance}-background-strong`,
+          },
+          medium: {
+            color: `nonSemantic-${basicAppearance}-content-primary`,
+            backgroundColor: `nonSemantic-${basicAppearance}-background-muted`,
+          },
+          low: {
+            color: `nonSemantic-${basicAppearance}-content-primary`,
+            backgroundColor: `nonSemantic-${basicAppearance}-background-onSubtle`,
+          },
+        },
+      ])
+    ),
+  },
+  intent: {
+    ...fromEntries(
+      basicIntents.map((basicIntent) => [
+        basicIntent,
+        {
+          high: {
+            color: `content-primary${
+              basicIntent !== 'warning' ? '-inverted' : ''
+            }` as SystemProps['color'],
+            backgroundColor: `background-${basicIntent}-strong`,
+          },
+          medium: {
+            color: `content-${
+              basicIntent !== 'neutral' ? `${basicIntent}-` : ''
+            }primary` as SystemProps['color'],
+            backgroundColor: `background-${basicIntent}-muted`,
+          },
+          low: {
+            color: `content-${
+              basicIntent !== 'neutral' ? `${basicIntent}-` : ''
+            }-primary` as SystemProps['color'],
+            backgroundColor: `background-${basicIntent}-onSubtle`,
+          },
+        },
+      ])
+    ),
+  },
+  display: 'flex',
+  w: 'fit-content',
+  h: 'xSmall',
+  borderRadius: 'large',
+  flexShrink: 0,
+  alignItems: 'center',
+  label: {
+    padding: 'component-padding-small',
+  },
+  icon: {
+    padding: 'component-padding-xSmall',
+  },
+  gap: 'component-gap-xSmall',
+} as const satisfies Omit<SystemProps<Theme>, 'appearance' | 'padding'> &
+  Config;
