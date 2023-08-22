4e4935d61679353ae8f21662d103f2a99ed5aa83
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2023-08-22 15:04:33 +0200

    chore: NO-JIRA prettier write

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,21 +1,21 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    plugins: [
-        {
-            rules: {
-                "jira-ticket": ({ subject }) => {
-                    const shouldContainJiraTicket = /TET-[0-9]{2,}/
-                    const orNoJira = /NO-JIRA/
-                    return [
-                        shouldContainJiraTicket.test(subject) || orNoJira.test(subject),
-                        `Your subject should contain Jira ticket (TET-XX) or NO-JIRA`,
-                    ]
-                },
-            },
+  extends: ['@commitlint/config-conventional'],
+  plugins: [
+    {
+      rules: {
+        'jira-ticket': ({ subject }) => {
+          const shouldContainJiraTicket = /TET-[0-9]{2,}/;
+          const orNoJira = /NO-JIRA/;
+          return [
+            shouldContainJiraTicket.test(subject) || orNoJira.test(subject),
+            `Your subject should contain Jira ticket (TET-XX) or NO-JIRA`,
+          ];
         },
-    ],
-    rules: {
-        "jira-ticket": [2, "always"],
-        "subject-case": [0]
+      },
     },
-}
+  ],
+  rules: {
+    'jira-ticket': [2, 'always'],
+    'subject-case': [0],
+  },
+};
