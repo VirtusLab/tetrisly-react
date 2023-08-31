c962708f4c1cbbb7b4ffa23803e06d549e1cae2c
Mateusz Kleszcz <mateusz.kacper.kleszcz@gmail.com>
2023-08-31 12:40:39 +0200

    Feat/861n1x9f6/toast (#32)
    * feat: add toast
    
    * feat: add tests, refactor styles
    
    * fix: fix tsconfig
    
    * Feat/tet 71/icon button (#31)
    
    * TET-71 feat: init icon button component
    
    * feat: add styling, typing to IconButton
    
    * feat: add styling config, fix Loader color in Button
    
    * chore: TET-71 bump tetrisly-icons to fix icons
    
    * feat: TET-69 add close button
    
    * feat: TET-69 add icon button to toast
    
    * fix: TET-69 review changes
    
    * fix: TET-69 fix icon button background color:
    
    * fix: TET-69 review changes
    
    * fix: TET-69 fix tsconfig
    
    ---------
    
    Co-authored-by: Mateusz Wlekliński <114148518+mwleklinskiVL@users.noreply.github.com>

---
+++ src/components/Toast/Toast.props.ts
@@ -0,0 +1,22 @@
+import { MouseEvent } from 'react';
+
+import { ToastConfig } from './Toast.styles';
+import { ToastIntent } from './types';
+import { ButtonProps } from '../Button';
+
+import { Emphasis } from '@/types/Emphasis';
+import { DeepPartial } from '@/utility-types/DeepPartial';
+
+export type Action = Pick<
+  ButtonProps<'bare'>,
+  'label' | 'onClick' | 'onFocus' | 'onBlur'
+>;
+
+export type ToastProps = {
+  text: string;
+  emphasis?: Emphasis;
+  intent?: ToastIntent;
+  action?: Action | [Action, Action | never];
+  onCloseClick?: (e: MouseEvent<HTMLButtonElement>) => void;
+  custom?: DeepPartial<ToastConfig>;
+};
