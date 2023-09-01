578ee54adb5b9f380e8612c5ca2d0e853b3ad747
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2023-09-01 15:04:17 +0200

    refactor: TET-128 Helper Text

--- src/components/HelperText/stylesBuilder/stylesBuilder.ts
+++ src/components/HelperText/stylesBuilder/stylesBuilder.ts
@@ -1,16 +1,14 @@
 import { merge } from 'lodash';
 
 import { HelperTextProps } from '../HelperText.props';
-import { defaultConfig, resolveIconName } from '../HelperText.styles';
+import { defaultConfig } from '../HelperText.styles';
 import { HelperTextIntent } from '../HelperTextIntent.type';
 
 import { BaseProps } from '@/types/BaseProps';
-import { IconName } from '@/utility-types/IconName';
 
 type HelperTextStyleBuilder = {
   container: BaseProps;
   iconContainer: BaseProps;
-  icon: { name: IconName<16> };
 };
 
 export const stylesBuilder = (
@@ -24,16 +22,11 @@ export const stylesBuilder = (
   } = merge(defaultConfig, custom);
   const { iconContainer } = innerElements;
 
-  const name = resolveIconName(intent);
-
   return {
     container: {
       ...containerIntent[intent],
       ...container,
     },
     iconContainer,
-    icon: {
-      name,
-    },
   };
 };
