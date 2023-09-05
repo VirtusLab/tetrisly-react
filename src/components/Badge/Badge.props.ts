b1b93050e0bf6b16782915af16dde53bdc9e2645
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2023-09-05 09:56:54 +0200

    refactor: TET-128 Badge

--- src/components/Badge/Badge.props.ts
+++ src/components/Badge/Badge.props.ts
@@ -1,18 +1,20 @@
 import { BadgeConfig } from './Badge.styles';
+import { BadgeAppearance } from './BadgeAppearance.type';
+import { BadgeEmphasis } from './BadgeEmphasis.type';
+import { BadgeIntent } from './BadgeIntent.type';
 
-import type { Appearance, BasicIntent } from '@/types';
 import { DeepPartial } from '@/utility-types/DeepPartial';
 import { IconName } from '@/utility-types/IconName';
 import { Or } from '@/utility-types/Or';
 
 export type BadgeProps = Or<
-  { appearance: Appearance },
-  { intent: BasicIntent }
+  { appearance?: BadgeAppearance },
+  { intent?: BadgeIntent }
 > &
   Or<
     { label: string; beforeIcon?: IconName<16>; afterIcon?: IconName<16> },
     { icon: IconName<16> }
   > & {
-    emphasis?: 'high' | 'medium' | 'low';
+    emphasis?: BadgeEmphasis;
     custom?: DeepPartial<BadgeConfig>;
   };
---
+++ src/components/Badge/BadgeEmphasis.type.ts
@@ -0,0 +1,1 @@
+export type BadgeEmphasis = 'high' | 'medium' | 'low';
