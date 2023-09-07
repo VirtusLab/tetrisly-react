067ad071e27dc2a6f12239449e286f69a6a67033
Mateusz Wlekliński <114148518+mwleklinskiVL@users.noreply.github.com>
2023-09-07 07:45:52 +0200

    TET-72 SocialButton (#36)
    * chore: TET-72 init component files
    
    * chore: TET-71 export component from index.ts
    
    * feat: TET-72 add svg with correct fill to socials
    
    * refactor: TET-72 move icons to seperate components and create stories
    
    * fix: NO-JIRA fix multiple exports error
    
    * fix: TET-72 fix styling for different states
    
    * fix: TET-72 fix loader for Google issue
    
    * test: TET-72 add tests
    
    * test: TET-72 test using customPropTester, test social icons
    
    * chore: TET-72 remove DeepPartial
    
    * chore: TET-72 omit disabled from social button
    
    * feat: TET-72 add label props, adjust test
    
    * fix: TET-72 change label props and adjust tests
    
    * fix: TET-72 fix build
    
    * fix: TET-72 bring back Partial
    
    * fix: TET-72 rename rest to restProps

---
+++ src/components/SocialButton/SocialButton.tsx
@@ -0,0 +1,51 @@
+import { FC, useMemo } from 'react';
+
+import { SocialButtonProps } from './SocialButton.props';
+import { socials } from './socials';
+import { stylesBuilder } from './stylesBuilder';
+
+import { tet } from '@/tetrisly';
+import { MarginProps } from '@/types';
+
+export const SocialButton: FC<SocialButtonProps & MarginProps> = ({
+  platform,
+  appearance = 'primary',
+  custom,
+  state,
+  label,
+  ...restProps
+}) => {
+  const social = socials[platform];
+
+  const [appearancesStyles, styles] = useMemo(
+    () => stylesBuilder(custom, appearance),
+    [appearance, custom],
+  );
+  return (
+    <tet.button
+      data-testid="social-button"
+      {...styles}
+      {...appearancesStyles}
+      {...restProps}
+      disabled={['disabled', 'loading'].includes(state ?? '')}
+      data-state={state}
+      style={{ textUnderlineOffset: '3px', textDecorationThickness: '1px' }}
+      bg={appearance === 'primary' ? social.color : 'action-inverted-normal'}
+    >
+      <social.Icon
+        loading={state === 'loading'}
+        fill={appearance === 'primary' ? 'white' : social.color}
+      />
+
+      <tet.span
+        color={
+          appearance === 'primary'
+            ? 'content-primary-inverted'
+            : 'action-neutral-normal'
+        }
+      >
+        {label}
+      </tet.span>
+    </tet.button>
+  );
+};
---
+++ src/components/SocialButton/socials/socials.ts
@@ -0,0 +1,43 @@
+import { FC } from 'react';
+
+import { Apple } from './Apple';
+import { Facebook } from './Facebook';
+import { Figma } from './Figma';
+import { Github } from './Github';
+import { Google } from './Google';
+import { SocialProps } from './SocialProps';
+import { Twitter } from './Twitter';
+import { SocialButtonProps } from '../SocialButton.props';
+
+export const socials = {
+  Apple: {
+    Icon: Apple,
+    color: '#000',
+  },
+  Facebook: {
+    Icon: Facebook,
+    color: '#1977F3',
+  },
+  Figma: {
+    Icon: Figma,
+    color: '#000',
+  },
+  Github: {
+    Icon: Github,
+    color: '#000',
+  },
+  Google: {
+    Icon: Google,
+    color: '#468BEF',
+  },
+  X: {
+    Icon: Twitter,
+    color: '#000',
+  },
+} as const satisfies Record<
+  SocialButtonProps['platform'],
+  {
+    Icon: FC<SocialProps>;
+    color: string;
+  }
+>;
