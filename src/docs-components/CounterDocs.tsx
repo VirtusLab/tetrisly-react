db68851c31257305975adc9db73e71381bdeb85c
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-09-19 14:03:51 +0200

    feat: TET-323

--- src/docs-components/CounterDocs.tsx
+++ src/docs-components/CounterDocs.tsx
@@ -1,3 +1,57 @@
+import { capitalize } from 'lodash';
+
+import { SectionHeader } from './common/SectionHeader';
+import { States } from './common/States';
+
+import { Counter } from '@/components/Counter';
 import { tet } from '@/tetrisly';
 
-export const CounterDocs = () => <tet.div />;
+const emphases = ['high', 'low'] as const;
+const appearances = [
+  'default',
+  'red',
+  'grey',
+  'blue',
+  'green',
+  'orange',
+] as const;
+
+export const CounterDocs = () => (
+  <>
+    {emphases.map((emphasis) => (
+      <tet.section
+        key={emphasis}
+        py="500"
+        borderBottomWidth="1px"
+        borderBottomColor="#9747FF"
+      >
+        <SectionHeader variant="H1" as="h2">
+          {capitalize(emphasis)} Emphasis
+          <tet.div py="500">
+            <States
+              states={appearances.map(capitalize)}
+              itemWidth="96px"
+              gap="40px"
+            />
+            <tet.div display="flex" gap="40px">
+              {appearances.map((appearance) => (
+                <tet.div
+                  display="flex"
+                  flexBasis="96px"
+                  flexShrink="0"
+                  flexGrow="1"
+                >
+                  <Counter
+                    number={24}
+                    emphasis={emphasis}
+                    appearance={appearance}
+                  />
+                </tet.div>
+              ))}
+            </tet.div>
+          </tet.div>
+        </SectionHeader>
+      </tet.section>
+    ))}
+  </>
+);
