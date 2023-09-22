80e33dcc40499500bf0cab25a71018b7536cefda
mwleklinskiVL <mwleklinski@virtuslab.com>
2023-09-22 15:03:42 +0200

    feat: TET-327 add inline message docs

---
+++ src/docs-components/common/Cols.tsx
@@ -0,0 +1,29 @@
+import { GapProps } from '@xstyled/styled-components';
+import { FC } from 'react';
+
+import { tet } from '@/tetrisly';
+import { MarginProps } from '@/types/MarginProps';
+import { PaddingProps } from '@/types/PaddingProps';
+
+type ColsProps = {
+  columns?: number;
+  children: React.ReactNode;
+  gap?: GapProps['gap'];
+} & PaddingProps &
+  MarginProps;
+
+export const Cols: FC<ColsProps> = ({
+  columns = 2,
+  children,
+  gap,
+  ...restProps
+}) => (
+  <tet.div
+    display="grid"
+    gridTemplateColumns={`repeat(${columns}, 1fr)`}
+    gap={gap}
+    {...restProps}
+  >
+    {children}
+  </tet.div>
+);
