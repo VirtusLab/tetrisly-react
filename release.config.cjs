0e4a2fb7bf27291d9154b0af307ffe02fbf11dee
Wojciech Kudyniuk <wojciech.kudyniuk@gmail.com>
2023-10-04 12:58:34 +0200

    chore: semantic release

--- release.config.cjs
+++ release.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ['main', {name: 'release-candidate', prerelease: true}],
+    branches: ['main', {name: 'release-candidate', prerelease: "rc"}],
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
