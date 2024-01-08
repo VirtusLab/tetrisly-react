3306b7d7373c271c571f27710d7cff9f03799de2
Adrian Potępa <adrian.potepa@gmail.com>
2024-01-08 09:36:34 +0100

    fix: removed storybook analytics addon and install GA script manually TET-592

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,9 +1,3 @@
-declare global {
-  interface Window {
-    STORYBOOK_GA_ID: string;
-  }
-}
-
 import { addons } from '@storybook/manager-api';
 
 import lightTheme from './lightTheme';
@@ -11,5 +5,3 @@ import lightTheme from './lightTheme';
 addons.setConfig({
   theme: lightTheme,
 });
-
-window.STORYBOOK_GA_ID = 'G-3011WK9KYZ';
