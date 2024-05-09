5be1bfc149d93d32e2b80fab48412b676cb0ad6d
KSzarek <karolina.gutka.97@gmail.com>
2024-05-09 22:45:58 +0200

    feat: TET-894 unify components folders

---
+++ src/components/HelperText/stylesBuilder.ts
@@ -0,0 +1,32 @@
+import { HelperTextProps } from './HelperText.props';
+import { defaultConfig } from './HelperText.styles';
+import { HelperTextIntent } from './HelperTextIntent.type';
+
+import { mergeConfigWithCustom } from '@/services';
+import { BaseProps } from '@/types/BaseProps';
+
+type HelperTextStyleBuilder = {
+  container: BaseProps;
+  iconContainer: BaseProps;
+};
+
+export const stylesBuilder = (
+  intent: HelperTextIntent,
+  custom: HelperTextProps['custom'],
+): HelperTextStyleBuilder => {
+  const {
+    innerElements,
+    intent: containerIntent,
+    ...container
+  } = mergeConfigWithCustom({ defaultConfig, custom });
+
+  const { iconContainer } = innerElements;
+
+  return {
+    container: {
+      ...containerIntent[intent],
+      ...container,
+    },
+    iconContainer,
+  };
+};
---
+++ src/components/Tag/stylesBuilder.ts
@@ -0,0 +1,36 @@
+import type { TagProps } from './Tag.props';
+import { defaultConfig } from './Tag.styles';
+
+import { mergeConfigWithCustom } from '@/services';
+import type { BaseProps } from '@/types/BaseProps';
+
+type TagStylesBuilder = {
+  container: BaseProps;
+  label: BaseProps;
+  avatar: BaseProps<'appearance'>;
+  closeButton: BaseProps<'appearance'>;
+};
+export const stylesBuilder = (
+  custom: TagProps['custom'],
+  hasOnClick?: boolean,
+): TagStylesBuilder => {
+  const {
+    hasOnClick: hasOnClickStyles,
+    innerElements: {
+      label,
+      closeButton,
+      beforeComponent: { avatar },
+    },
+    ...container
+  } = mergeConfigWithCustom({ defaultConfig, custom });
+
+  return {
+    container: {
+      ...container,
+      ...(hasOnClick && hasOnClickStyles),
+    },
+    label,
+    avatar,
+    closeButton,
+  };
+};
