d48dd7fb67a026ae550d739717b3d1d292bdfa2a
Karolina Szarek <74671633+karolinaszarek@users.noreply.github.com>
2024-09-02 11:55:47 +0200

    feat: TET-848 add button group (#160)
    * feat: TET-848 add ButtonGroup component
    
    * feat: TET-848 add tests and fix a bug
    
    * feat: TET-848 add export
    
    * feat: TET-848  resolve build problems

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './components/Avatar';
 export * from './components/Badge';
 export * from './components/BooleanPill';
 export * from './components/Button';
+export * from './components/ButtonGroup';
 export * from './components/Checkbox';
 export * from './components/CheckboxGroup';
 export * from './components/CornerDialog';
